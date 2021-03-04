43c0cccc471ae4a1da6a58b46534db5d2b22ffb7
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-04 11:21:13 +0700

    new

---
+++ app/src/main/java/com/aditya/camping/model/DataPlace.kt
@@ -0,0 +1,4 @@
+package com.aditya.camping.model
+
+object DataPlace {
+}
---
+++ app/src/main/java/com/aditya/camping/model/ModelPlace.kt
@@ -0,0 +1,8 @@
+package com.aditya.camping.model
+
+data class ModelPlace (
+    val name: String = "",
+    val description: String = "",
+    val isFavorited: Boolean = false,
+    val photo: Int = 0
+)
