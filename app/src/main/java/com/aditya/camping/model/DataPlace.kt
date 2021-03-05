d9b8fe54acb91f8dc6187a186888a81b606e1c15
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-06 06:55:33 +0700

    new

--- app/src/main/java/com/aditya/camping/model/DataPlace.kt
+++ app/src/main/java/com/aditya/camping/model/DataPlace.kt
@@ -1,4 +1,36 @@
 package com.aditya.camping.model
 
+import com.aditya.camping.R
+
 object DataPlace {
+    private val name = arrayOf(
+            "Belitung island",
+            "Mount Bromo",
+            "Bunaken Islang",
+            "Mount Kelimutu",
+            "Komodo Island",
+            "Padar Island",
+            "Raja Ampat Archipelago",
+            "Mount Rinjani",
+            "Mount Situ Gunung",
+            "Toba Lake"
+    )
+
+    private val photo = intArrayOf(
+            R.drawable.belitung,
+            R.drawable.bromo,
+            R.drawable.bunaken,
+            R.drawable.kelimutu_mount,
+            R.drawable.komodo,
+            R.drawable.pulau_padar,
+            R.drawable.raja_ampat,
+            R.drawable.rinjani_mount,
+            R.drawable.situ_gunung,
+            R.drawable.toba
+    )
+
+    private val description = arrayOf(
+            "",
+            ""
+    )
 }
