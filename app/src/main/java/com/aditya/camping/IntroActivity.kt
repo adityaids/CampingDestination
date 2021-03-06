f03171046302b4e22d43ea80f166d28aaafe118b
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-06 15:26:08 +0700

    new

--- app/src/main/java/com/aditya/camping/IntroActivity.kt
+++ app/src/main/java/com/aditya/camping/IntroActivity.kt
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+        supportActionBar?.hide()
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+            finish()
         }, 10000L)
     }
 }
