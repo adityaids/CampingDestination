1d27a3e9c5bab4dd5b3a8f3a372395ff63341ee5
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-06 07:57:57 +0700

    new

--- app/src/main/java/com/aditya/camping/model/DataPlace.kt
+++ app/src/main/java/com/aditya/camping/model/DataPlace.kt
@@ -6,7 +6,7 @@ object DataPlace {
     private val name = arrayOf(
             "Belitung island",
             "Mount Bromo",
-            "Bunaken Islang",
+            "Bunaken Island",
             "Mount Kelimutu",
             "Komodo Island",
             "Padar Island",
@@ -30,7 +30,10 @@ object DataPlace {
     )
 
     private val description = arrayOf(
-            "",
+            "Discover the beauty nature of Belitung Island, Belitung island has become a new favorite island-themed holiday destination among tourists located south-east off the larger island of Sumatra.  With its impeccable pristine beaches, blue waters, and natural granite rock structures, its hard not to linger on its beauty.",
+            "Discover the magnificent beauty mount Bromo, the most popular tourist destination in East Java and always filled with domestic and international tourists, The name Bromo is said to derive from Brama (Brahma), a Hindu God. Until now, Mt. Bromo is still considered a holy place for Hindus, making it a location for the annual Yadnya Kasada or Kasodo ceremony. People around Mount Bromo will celebrate the Kasodo festivals annually by presenting offerings like vegetables, chickens and money dedicated to the Gods and thrown into the crater of Mount Bromo as an entity of gratitude to the Almighty.",
+            "Discover the beauty nature of Bunaken Island, Bunaken Island is part of the Bunaken National Marine Park. Bunaken is located at the northern tip of the island of Sulawesi, Indonesia. It belongs administratively to the municipality of Manado. Scuba diving attracts many visitors to the island.",
+            "Discover the beauty landscape of Kelimutu Mount, This mountain has three crater lakes at the peak. These lakes are known as the Three Color Lakes because it has three different colors, which are red, blue and white. Even so, these colors always change over time.",
             ""
     )
 }
