4ea66d912e601e5f25ea1e479ebd7eecf6f75758
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-06 15:05:05 +0700

    new

--- app/src/main/java/com/aditya/camping/IntroActivity.kt
+++ app/src/main/java/com/aditya/camping/IntroActivity.kt
@@ -1,11 +1,18 @@
 package com.aditya.camping
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }, 10000L)
     }
 }
