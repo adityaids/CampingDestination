86eb1c7801cad6d48ec6a58c5c28532d06ebb022
Aditya Purnama <aditya.purnamaaa@gmail.com>
2021-03-09 13:54:24 +0700

    new

--- app/src/main/java/com/aditya/camping/MainActivity.kt
+++ app/src/main/java/com/aditya/camping/MainActivity.kt
@@ -3,16 +3,12 @@ package com.aditya.camping
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Pair
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.app.ActivityOptionsCompat
-import androidx.core.view.get
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aditya.camping.model.DataPlace
